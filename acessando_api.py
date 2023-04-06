f90d614c286fc1a1df008a7317b186f7ddb0d84a
Michele de Barros Santos <michele.fenix.17@gmail.com>
2023-04-05 21:26:34 -0300

    04,1

--- acessando_api.py
+++ acessando_api.py
@@ -6,6 +6,6 @@ url = 'http://127.0.0.1:5000/api/funcionarios'
 response = requests.get(url)
 
 if response.status_code != 200:
-    print("Erro ao acessar a API " + response.json().__str__())
+    print("Erro ao acessar a API " + response.text)
 
 print(response.json())
